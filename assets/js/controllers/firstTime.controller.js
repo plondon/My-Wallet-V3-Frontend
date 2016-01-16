e6048b58a1c012f02935c06dfc227967f39b391e
plondon <plondon@gm.slc.edu>
2016-01-16 16:13:59 -0500

    feat(LoginModal): iterate gif only once and launch blocket into space

--- assets/js/controllers/firstTime.controller.js
+++ assets/js/controllers/firstTime.controller.js
@@ -2,9 +2,22 @@ angular
   .module('walletApp')
   .controller("FirstTimeCtrl", FirstTimeCtrl);
 
-function FirstTimeCtrl($scope, $uibModalInstance, firstTime) {
+function FirstTimeCtrl($scope, $timeout, $uibModalInstance, firstTime) {
   $scope.firstTime = firstTime;
   $scope.ok = () => {
   	$uibModalInstance.close(firstTime);
   }
+
+  $scope.rocket = {
+    finished: false,
+    launching: false
+  }
+
+  $timeout(() => {
+    $scope.rocket.launching = true;
+  }, 4780)
+
+  $timeout(() => {
+    $scope.rocket.finished = true;
+  }, 6040)
 }
